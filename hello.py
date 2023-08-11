b77536926caa020caababe461b493c743db539bc
Mariam Gamal <mariam1372010m@icloud.com>
2023-08-11 21:07:19 +0000

    m

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # ask users for their name
-name = input(" what's your name? ")
+name = input(" what's your name? ").strip().title()
+
+#split user's name into first name 
+first, last = name.split(" ")
 
 # say hello to user
-print(f"hello, {name}") 
+print(f"hello, {first}")
